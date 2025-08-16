92063e0a6f85e3e2d3522911be46eb716b947594
fangzhen <fangzhen@live.com>
2025-08-16 10:38:44 +0800

    Add SimpleTextUI class for app-pattern text ui

--- kissllm/io.py
+++ kissllm/io.py
@@ -10,21 +10,27 @@ class OutputMetadata:
     pass
 
 
+class SimpleTextUI:
+    def __init__(self, name, input_generator=None):
+        self.input_generator = input_generator or IOChannel.simple_input_generator()
+        self.name = name
+        self.io_channel = IOChannel(name, self)
+
+    def run(self):
+        raise NotImplementedError()
+
+
 class IOChannel:
-    def __init__(self, channel_type):
+    def __init__(self, channel_type, app):
         self.channel_type = channel_type
+        self.app = app
 
     def create_sub_channel(self, channel_type, title=""):
-        print()
-        return self.__class__(channel_type)
+        return self.__class__(channel_type, self.app)
 
     async def read(self):
-        try:
-            while True:
-                line = input("User (Ctrl+D to quit): ")
-                yield line
-        except EOFError:
-            pass
+        async for user_input in self.app.input_generator:
+            yield user_input
 
     async def write(self, content, metadata: OutputMetadata | None = None):
         if self.channel_type == IOTypeEnum.prompt_message:
@@ -33,6 +39,15 @@ class IOChannel:
             print(content, end="", flush=True)
         else:
             print(content)
+
+    @staticmethod
+    async def simple_input_generator():
+        try:
+            while True:
+                line = input("\nUser (Ctrl+D to quit): ")
+                yield line
+        except EOFError:
+            pass
 
 
 class IOTypeEnum(str, Enum):
